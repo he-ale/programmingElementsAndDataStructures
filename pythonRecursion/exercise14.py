1e91f34aba7428f4129157313a2424f1f1923d41
he-ale <he.ale.cladera@gmail.com>
2026-06-15 23:38:44 -0400

    061520262338

---
+++ pythonRecursion/exercise14.py
@@ -0,0 +1,20 @@
+
+
+def exercise14(n: int):
+    digit= n%10
+    if (digit%2==1):
+        return False
+    
+    if (n==0):
+        return True
+    
+    return exercise14(n//10)
+
+def other(n:int):
+    res= True
+    while(n>0 and res):
+        res= (n%10)%2==0
+        n= n//10
+    return res
+
+print(other(212))
